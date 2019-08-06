6c67983fa61908900d4e80d268eca4c7404dd103
michellejkim <michellekimphd@gmail.com>
2019-08-06 08:27:04 -0400

    Aug 6 Class Module

--- Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
+++ Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
@@ -1,11 +1,11 @@
 """stores the yelp api client id and api key in a separate file for security and best practice"""
 
 def return_api_key():
-"""returns api key for yelp api"""
-    api_key = ''"
+    """returns api key for yelp api"""
+    api_key = '66kq78iVk9QpkN2JE9zjpygQwR2L0Ttqk4cA3_FY9mrFCTCD7ULrqKeT6CO0xmY5FOdMdTa-_4q53okOuzh_AHlF8QahVaJLjDy3kyxpPwClmTSiLA_8-hqNgmhIXXYx'
     return api_key
 
 def return_client_id():
-"""returns client id for yelp api"""
-    client_id = ''
+    """returns client id for yelp api"""
+    client_id = 'c657UKhHCkmJ4b-ylLkh0Q'
     return client_id
